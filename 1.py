dc9ff786cc05b8c76706eff4fc2386f2f6dd2d6b
t-kotera <t-kotera@fabrica-com.co.jp>
2026-08-13 09:38:10 +0900

    いらない部分を消して新しい機能を追加しました

--- 1.py
+++ 1.py
@@ -1,8 +1,4 @@
-print("hello world")
 
-print("xcvbn")
-
-print("asdfg")
 
 a=12345
 c=12345
@@ -18,3 +14,10 @@ x=100
 z=100
 free(x,z)
 print(x,z)
+
+
+x=(1,2,3,4,5)
+s=0
+for i in x:
+    s=i*i
+    print(s)
